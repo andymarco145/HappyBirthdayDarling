08ff306567c1a4b5998fc28220c055facdad846c
andymarco145 <tduc1405@gmail.com>
2024-08-09 15:54:55 +0700

    completed

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+// Get the audio element
+var backgroundMusic = document.getElementById("backgroundMusic");
+var newBackgroundMusic = document.getElementById("newBackgroundMusic");
+
+backgroundMusic.volume = 0.5;
+newBackgroundMusic.volume = 0.3;
+
 function showMessage() {
     var heart = document.getElementById("heart");
     var messageCard = document.getElementById("messageCard");
     var button = document.querySelector(".press-btn");
     var playVideoBtn = document.getElementById("playVideoBtn");
-    
+
     button.style.display = "none"; // Hide the button
     
     heart.style.display = "block";
@@ -11,17 +18,26 @@ function showMessage() {
         heart.style.display = "none";
         messageCard.style.transform = "translate(-50%, -50%) scale(1)";
         playVideoBtn.style.display = "block"; // Show the video button
-    }, 6000); // Heart animation for 1 second
+    }, 17000); // Heart animation for 6 seconds
+
+    // Play background music after clicking the button
+    backgroundMusic.play();
 }
 
 function playVideo() {
     var video = document.getElementById("birthdayVideo");
     var thankYouBtn = document.getElementById("thankYouBtn");
+
+    // Pause background music when video plays
+    backgroundMusic.pause();
+    
     video.style.display = "block";
     video.play();
 
     video.onended = function() {
         thankYouBtn.style.display = "block"; // Show the thank you button when the video ends
+        // Resume background music after video ends
+        // backgroundMusic.play();
     };
 }
 
@@ -32,6 +48,8 @@ function showThankYou() {
 
     thankYouImg.style.display = "block";
     reloadBtn.style.display = "block";
+
+    newBackgroundMusic.play();
 }
 
 function reloadPage() {
